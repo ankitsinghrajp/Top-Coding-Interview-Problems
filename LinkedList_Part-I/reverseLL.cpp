1afcd02fc8889b7e0b3597d2716f322e422ab2f7
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2026-03-12 21:55:20 +0530

    more problems added

---
+++ LinkedList_Part-I/reverseLL.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+       int data;
+       Node* next;
+};
+
+void printLinkedList(Node* Head){
+    Node* temp = Head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    cout<<endl;
+}
+
+
+Node* reverseLinkedList(Node* Head){
+   Node* curr = Head;
+   Node* prev = nullptr;
+   Node* next = nullptr;
+
+   while(curr!=nullptr){
+    next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+   }
+
+   return prev;
+}
+
+int main(){
+   Node* Head = new Node();
+   Head->data = 1;
+   
+   Node* second = new Node();
+   Node* third = new Node();
+   Node* fourth = new Node();
+   Node* fifth = new Node();
+   
+   second->data = 2;
+   Head->next = second;
+   
+   third->data = 3;
+   second->next = third;
+
+   fourth->data = 4;
+   third->next = fourth;
+
+   fifth->data = 5;
+   fourth->next = fifth;
+   fifth->next = nullptr;
+
+   printLinkedList(Head);
+
+   Node* reversed = reverseLinkedList(Head);
+   printLinkedList(reversed);
+
+    return 0;
+}
