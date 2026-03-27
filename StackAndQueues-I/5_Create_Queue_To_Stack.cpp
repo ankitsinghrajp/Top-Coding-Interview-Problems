e6e1e9957de9670e9481d8cd6eda5eb349d0edfb
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2026-03-27 22:51:50 +0530

    Implemented stack using queue

---
+++ StackAndQueues-I/5_Create_Queue_To_Stack.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Stack{
+
+   public:
+   queue<int> q;
+    void push(int x){
+      q.push(x);
+      int s = q.size();
+      for(int i=0;i<s-1;i++){
+          q.push(q.front());
+          q.pop();
+      }
+      
+   }
+
+   void pop(){
+     q.pop();
+   }
+
+   int top(){
+    return q.front();
+   }
+   int size(){
+    return q.size();
+   }
+};
+int main(){
+    Stack st;
+    st.push(5);
+    st.push(10);
+    st.push(15);
+    st.push(20);
+
+    cout<<"The top element is: "<<st.top()<<endl;
+    st.pop();
+    cout<<"The top element is: "<<st.top()<<endl;
+    st.pop();
+    st.pop();
+    cout<<"The top element is: "<<st.top()<<endl;
+    cout<<"The size of the stack is: "<<st.size()<<endl;
+    return 0;
+}
