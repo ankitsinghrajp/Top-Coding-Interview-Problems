35322ce0811293dfe51e5a679d60b6794957c68f
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2026-04-08 21:25:54 +0530

    Inorder traversal

---
+++ BST-I/2_InOrder_Traversal/inorder.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+      int data;
+      Node* left;
+      Node* right;
+
+    Node(int value){
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BinaryTree{
+    public:
+        Node* create(string msg){
+            int x;
+            cout<<msg<<" ";
+            cin>>x;
+
+            if(x == -1) return NULL;
+
+            Node* temp = new Node(x);
+            temp->left = create("Enter the left child of: "+to_string(temp->data)+": ");
+            temp->right = create("Enter the right child of: "+to_string(temp->data)+": ");
+            return temp;
+        }
+};
+
+void PreOrder(Node* root){
+
+    if(root == NULL) return;
+
+    cout<<root->data<<" ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+
+}
+int main(){
+    
+    BinaryTree bt;
+    Node* root = bt.create("Enter the root node: ");
+
+    cout<<"The Inorder traversal of the tree is: "<<endl;
+    PreOrder(root);
+    return 0;
+}
